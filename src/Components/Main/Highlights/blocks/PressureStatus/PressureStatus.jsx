a2056d7909520f185d6355b49e428047a9403918
Ilchenko Daniil Yurievich <crocus161@gmail.com>
2021-11-04 11:17:11 +0200

    Fixed bugs with pressure points

--- src/Components/Main/Highlights/blocks/PressureStatus/PressureStatus.jsx
+++ src/Components/Main/Highlights/blocks/PressureStatus/PressureStatus.jsx
@@ -8,19 +8,22 @@ import styles from './PressureStatus.module.scss';
 const PressureStatus = ({pressure}) => {
     const { t } = useTranslation();
 
+    //format pressure from hectopascals to millimeters of water column
+    const formatPressure = (pressure / 1.3333).toFixed(0);
+
     let healthyPoint;
 
-    if(pressure <= 700 || pressure > 1000) healthyPoint = `${t('highlights.pressure-status.unhealthy')} 👎🏻`
-    else if(pressure > 900) healthyPoint = `${t('highlights.pressure-status.average')} 🙁`
-    else if(pressure > 800) healthyPoint = `${t('highlights.pressure-status.normal')} 🤙🏻`
-    else if(pressure > 765) healthyPoint = `${t('highlights.pressure-status.good')} 😀`
+    if(formatPressure <= 755 || formatPressure > 1000) healthyPoint = `${t('highlights.pressure-status.unhealthy')} 👎🏻`
+    else if(formatPressure > 900) healthyPoint = `${t('highlights.pressure-status.average')} 🙁`
+    else if(formatPressure > 800) healthyPoint = `${t('highlights.pressure-status.normal')} 🤙🏻`
+    else if(formatPressure > 755) healthyPoint = `${t('highlights.pressure-status.good')} 😀`
     
     return (
         <div className={main.highlight}>
             <h2 className={main.highlight__title}>
                 {t('highlights.pressure-status.title')}
             </h2>
-            <h2 className={main.highlight__value}>{pressure}</h2>
+            <h2 className={main.highlight__value}>{formatPressure}</h2>
             <h3 className={styles.highlight__unit}>{healthyPoint}</h3>
         </div>
     );
